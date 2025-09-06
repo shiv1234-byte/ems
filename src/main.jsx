6f3d4fd2714933513b7393349fe837421551cb5e
shiv1234-byte <139425771+shiv1234-byte@users.noreply.github.com>
2025-09-06 20:06:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,9 +9,9 @@ import AuthProvider from './context/AuthProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+   <BrowserRouter basename="/employee-management-system">
+  <App />
+</BrowserRouter>
 
   </React.StrictMode>,
 )
